e9be033d254de2a45e5fc4fc284c459ad18ee392
Jrcfencing <jrcfencing@outlook.com>
2026-07-20 22:09:01 +1000

    Commit - Added Index Fix

--- serve.bat
+++ serve.bat
@@ -1,9 +1,9 @@
 @echo off
 rem ── JRC Fencing local preview server ─────────────────────────
 rem Double-click this file, then open:
-rem   http://localhost:8123/JRC%%20Fencing.dc.html   (main site)
-rem   http://localhost:8123/measure-up.html          (3D measure-up)
+rem   http://localhost:8123/                 (main site)
+rem   http://localhost:8123/measure-up.html  (3D measure-up)
 rem Close this window to stop the server.
 cd /d "%~dp0"
-start "" "http://localhost:8123/JRC%%20Fencing.dc.html"
+start "" "http://localhost:8123/"
 python -m http.server 8123
